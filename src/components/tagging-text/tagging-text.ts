52620b9a4bf1c2f8ca466319edb050f5a8aa6bef
Rithie <euler.sistemas@gmail.com>
2017-01-15 11:04:09 -0200

    added readme file

--- src/components/tagging-text/tagging-text.ts
+++ src/components/tagging-text/tagging-text.ts
@@ -23,27 +23,48 @@ export class TaggingTextComponent {
 
 
   onKey(event:any) { // without type info
-    this.values += event.target.value + ' | ' + event.which;
+    
+    //this.values += event.target.value + ' | ' + event.which;
 
     let el = document.querySelector("#textarea input");
     let key = event.which;
     
     if (key == 188) {
-    //console.log(el.value.slice(0,-1));
-    //pegar o pai de el
-    var refenceNode = el.parentNode;
-    //console.log(refenceNode);
-    //criar elemento button
-    var button = document.createElement("button");
-    //inserir no button o texto digitado antes da vírgula(-1)
-    button.innerText = event.target.value.slice(0, -1);
-    //inserir no antes de el, o novo elemento button
-    refenceNode.insertBefore(button, el);
-    //limpar campo de texto
-    event.target.value = '';
-    //setar focus
-    event.target.focus();
-  }
+        //console.log(el.value.slice(0,-1));
+        //pegar o pai de el
+        var refenceNode = el.parentNode;
+        //console.log(refenceNode);
+        //criar elemento button
+        var button = document.createElement("button");
+        //inserir no button o texto digitado antes da vírgula(-1)
+        button.innerText = event.target.value.slice(0, -1);
+        //inserir no antes de el, o novo elemento button
+        refenceNode.insertBefore(button, el);
+        //limpar campo de texto
+        event.target.value = '';
+        //setar focus
+        event.target.focus();
+
+        this.delegate(document, "click", "button", (event) => {
+
+            event.target.style.display = 'none';
+        });
+    }
   }
 
+  delegate(el, evt, sel, handler) {
+      el.addEventListener(evt, (event) => {
+        var t = event.target;
+        while (t && t !== this) {
+          if (t.matches(sel)) {
+            handler.call(t, event);
+          }
+          t = t.parentNode;
+        }
+      });
+ }
+
 }
+
+
+
